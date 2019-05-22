62bdaeeaebe0bb6dd0f53441a250abed2e730d52
Ola Podsiadlik <alecs@op.pl>
2019-05-22 22:36:55 +0200

    Test player component

---
+++ src/components/Player.test.js
@@ -0,0 +1,26 @@
+import Player from './Player';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+  shallow(<Player />);
+});
+
+it('renders correct name', () => {
+    const playerNamePassed = 'Ania';
+    const playerComponent = shallow(<Player name={playerNamePassed} />);
+  
+    const playerNameRendered = playerComponent.find('.Player__name').text();
+  
+    expect(playerNameRendered).toEqual(playerNamePassed);
+  });
+
+  it('renders correct score', () => {
+    const playerScorePassed = 5;
+    const playerComponent = shallow(<Player score={playerScorePassed} />);
+  
+    const playerScoreRendered = Number(playerComponent.find('.Player__score').text());
+    
+  
+    expect(playerScoreRendered).toEqual(playerScorePassed);
+  });
